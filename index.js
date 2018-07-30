602c1c7f73ab92063443d47a4d1d919b69849ead
lolicon999 <appiedavid777@gmail.com>
2018-07-31 02:02:39 +0800

    ping

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-// Discord.js bot
+/*
+  A ping pong bot, whenever you send "ping", it replies "pong".
+*/
+
+// Import the discord.js module
 const Discord = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Discord.Client();
 
+// The token of your bot - https://discordapp.com/developers/applications/me
+const token = 'NDczNTIzNDMzMzE0MTg5MzEz.DkDTaw.RdeJTRCqtJ5JXzj4t9g5CHm4Lo0';
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+  console.log('I am ready!');
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
 });
 
-client.login(process.env.TOKEN);
+// Log our bot in
+client.login(token);
